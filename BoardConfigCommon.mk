be77fb0644bad608779200e6d7e7917d66fef88d
Alessandro Astone <ales.astone@gmail.com>
2019-09-10 09:57:50 +0200

    msm8916-common: add libbinder shim library
     * Commit f1cf11f1a4d5255554b49c6b299070676e53b72f of
       frameworks/native removed these support symbols
    
    Change-Id: I4fadd98c1ad11424b6ba5ff309d1e86dcf3cdca1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,14 +157,18 @@ BOARD_ROOT_EXTRA_FOLDERS := firmware persist fsg
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
-# Shims
+# SHIMS
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libandroid.so|libshim_ril.so \
+    /system/vendor/lib/libsensorndkbridge.so|libshim_ril.so \
     /system/vendor/lib/libmdmcutback.so|libqsap_shim.so \
-    /system/lib/libmot_sensorlistener.so|libsensor.so \
-    /system/lib/libjustshoot.so|libshims_camera.so \
-    /system/lib/libjustshoot.so|libjustshoot_shim.so \
-    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so
+    /system/vendor/lib/libjustshoot.so|libshims_camera.so \
+    /system/vendor/lib/libmot_sensorlistener.so|libshims_sensorlistener.so \
+    /system/vendor/lib/libmmcamera_wavelet_lib.so|libshim_atomic.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
+    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
+    /system/vendor/lib/libjustshoot.so|libjustshoot_shim.so \
+    /system/vendor/lib/libperipheral_client.so|libshim_binder.so
 
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
